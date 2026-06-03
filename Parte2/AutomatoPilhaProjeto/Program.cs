ba8df81bead8a5199142e3618f5a257f037becf7
Cauã de Moraes <cauamoraes76@gmail.com>
2026-06-03 20:30:29 -0300

    feat: teste do automato

--- Parte2/AutomatoPilhaProjeto/Program.cs
+++ Parte2/AutomatoPilhaProjeto/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AutomatoPilha automato = new AutomatoPilha();
+
+        automato.Simular("aaabbb");
+    }
+}
